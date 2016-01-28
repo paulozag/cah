0cfff3b837b9faa71e00624043d5b97701d4be7d
paulozag <ozag@comcast.net>
2016-01-27 20:15:18 -0800

    adds some default values to Round.

--- db/migrate/20160118011016_create_rounds.rb
+++ db/migrate/20160118011016_create_rounds.rb
@@ -3,11 +3,12 @@ class CreateRounds < ActiveRecord::Migration
     create_table :rounds do |t|
       t.string :phase, default: 'start'
       t.references :game
-      t.integer :round_number
+      t.integer :round_number, default: 1
       t.integer :judge_id
       t.integer :winner_id
-      t.references :question_card
-      t.references :answer_card
+      t.references :question_card, default: nil
+      t.references :answer_card, default: nil
+
       t.text :player_answers, hash: true, defalut: {}
 
       t.timestamps null: false
