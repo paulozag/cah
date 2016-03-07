b322bcd0b800ea13a16762588160922e7db036b4
paulozag <ozag@comcast.net>
2016-03-07 12:01:34 -0800

    deals new answer cards for players who have less than 10

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -112,6 +112,7 @@ class RoundsController < ApplicationController
     @round.save
     move_question_card_to_winners_hand
     move_played_answer_cards_to_discard_pile
+    deal_new_answer_cards_to_players
     create_next_round
   end
 
@@ -141,6 +142,16 @@ class RoundsController < ApplicationController
     player_count = @game.players.count
     judge_index = ((@game.round_number) -1) % player_count
     @game.players[judge_index].id
+  end
+
+  def deal_new_answer_cards_to_players
+    @game.players.each do |player|
+      (10 - player.answer_cards.count).times do
+        card = @game.draw_answer_card
+        card.player_id = player.id
+        card.save
+      end
+    end
   end
 
 end
